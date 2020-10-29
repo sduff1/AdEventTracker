d7c546e75d35320b6e30144c8f9cb0353e154e36
Francis Okereke <foker1@brockport.edu>
2020-10-29 15:04:29 -0400

    Update addPromotionForm.php
    Added validation to cover cases where nothing was entered.

--- addPromotionForm.php
+++ addPromotionForm.php
@@ -18,6 +18,44 @@ require('addPromotion.php');
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Add a Promotion</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script type = "text/javascript">
+        // Form validation for the following fields will come below ex. number,description etc.
+        function tvalidate() {
+
+            if (document.myForm.name.value == "") {
+                alert("Please provide the item name!");
+                document.myForm.Name.focus();
+                return false;
+            }
+
+            if (document.myForm.description.value == "") {
+                alert("Please provide the item description!");
+                document.myForm.description.focus();
+                return false;
+            }
+
+            if (document.myForm.amountOff.value == "") {
+                alert("Please provide the amount off!");
+                document.myForm.amountOff.focus();
+                return false;
+            }
+            if (document.myForm.type.value == "") {
+                alert("Please provide the type!");
+                document.myForm.type.focus();
+                return false;
+            }
+            if (document.myForm.cost.code == "") {
+                alert("Please provide the items' promo code!");
+                document.myForm.cost.focus();
+                return false;
+            }
+
+
+            return (true);
+        }
+
+
+    </script>
 </head>
 <body>
 <div class ="container">
@@ -26,7 +64,7 @@ require('addPromotion.php');
 
         <h1>Add a Promotion</h1>
 
-        <form method="POST" action="addPromotionForm.php">
+        <form method="POST" action="addPromotionForm.php"  name ="myForm" onsubmit = "return(tvalidate());">
 
             <label>Promotion Name</label>
             <input type = "text" name="name" class="form-control">
