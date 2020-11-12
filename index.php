323987d926c561f1890ad294d8f27d4f69967593
Anthony Gonzalez <anthonyg@Anthonys-MacBook-Pro.local>
2020-11-12 00:24:59 -0500

    Added Stlying to front page, revamped nav bar

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
  */
 ?>
 
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -14,14 +15,26 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Ad Event Tracker</title>
+    <link rel = "stylesheet" href="css/style.css"
+    <title></title>
 </head>
 <body>
-<div class="jumbotron jumbotron-fluid">
+<div id="showcase" >
     <div class="container">
-        <h1 class="display-4">Mock Demo Test</h1>
-        <p class="lead">Ad Event Track Mock Demo Test.</p>
+        <div class="row align-items-center">
+            <div class="col">
+                <h1 id = "title" class="mt-sm-5 font-weight-bold text-light">Ad Event Tracker </h1>
+                <hr class="hr-light">
+                <h6 class="mb-4 text-light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem repellendus quasi fuga nesciunt
+                    dolorum nulla magnam veniam sapiente, fugiat! Commodi sequi non animi ea dolor molestiae
+                    iste.</h6>
+            </div>
+        </div>
     </div>
 </div>
+
+<section id = "section-a">
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet aspernatur cum dolore dolores earum eveniet iure obcaecati, quaerat quasi quia quo sapiente totam voluptatum! Adipisci asperiores consectetur eligendi eveniet fugiat ipsum itaque laudantium quam quas quod. Animi ea facere nam odit quis, quos reiciendis rem sunt voluptate!</p>
+</section>
 </body>
 </html>
