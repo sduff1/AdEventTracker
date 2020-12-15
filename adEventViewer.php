70633205db0362388a857ce2a58ee7fc0fba37a3
sduff1 <71339826+sduff1@users.noreply.github.com>
2020-12-15 13:31:22 -0500

    Add files via upload

---
+++ adEventViewer.php
@@ -0,0 +1,89 @@
+<?php
+require('configuration/database.php');
+//Start session in order to send variable data to table.php in order to create table for item
+require('Promotion.php');
+require('Item.php');
+//session_start();
+
+
+if(isset($_POST['submit'])){
+
+    //Get item number entered by user
+    $event = mysqli_real_escape_string($conn, $_POST['event_code']);
+    ///echo $number;
+
+    //Create query
+    $query = "SELECT * FROM adevent WHERE event_code = '$event'";
+
+
+    //Get result
+    $result = mysqli_query($conn,$query);
+
+    //Fetch data
+    $post = mysqli_fetch_assoc($result);
+
+    //Get specific data from database
+    $code = $post['event_code'];
+    $name = $post['name'];
+    $start = $post['start_date'];
+    $end = $post['end_date'];
+    $desc = $post['description'];
+
+    $query1 = "SELECT PromoCode FROM adeventpromotion WHERE EventCode = '$event'";
+    $result1 = mysqli_query($conn, $query1);
+    $promos = [];
+    $i=0;
+    while($row = mysqli_fetch_assoc($result1))
+    {
+        $promos[$i] = $row;
+        $i++;
+    }
+    //Create session variable to be used in searchItemTable.php
+    $_SESSION['event_code'] = $code;
+    $_SESSION['name'] = $name;
+    $_SESSION['start_date'] = $start;
+    $_SESSION['end_date'] = $end;
+    $_SESSION['description'] = $desc;
+    $_SESSION['promos'] = $promos;
+
+    function promoitems($pcode)
+    {
+        $promocode = $pcode;
+        require('configuration/database.php');
+        $query2 = "SELECT ItemNumber FROM promotionitem WHERE PromoCode = '$promocode'";
+        return mysqli_fetch_assoc(mysqli_query($conn, $query2));
+    }
+    function getItem($inum)
+    {
+        require('configuration/database.php');
+        $num = $inum['ItemNumber'];
+        $query3 = "SELECT * FROM item WHERE item_number = '$num'";
+        $result3 = mysqli_query($conn,$query3);
+        $post3 = mysqli_fetch_assoc($result3);
+        $itemnumber = $post3['item_number'];
+        $itemdesc = $post3['item_description'];
+        $cat = $post3['category'];
+        $dept = $post3['department_name'];
+        $cost = $post3['purchase_cost'];
+        $retail = $post3['full_retail_price'];
+        return new item($itemnumber, $itemdesc, $cat, $dept, $cost, $retail);
+    }
+    function getPromotion($pcode)
+    {
+        $promocode = $pcode['PromoCode'];
+        require('configuration/database.php');
+        $result4 = mysqli_query($conn,"SELECT Name, Description, AmountOff, PromoType, PromoCode FROM promotion WHERE PromoCode = '$promocode'");
+        $post4 = mysqli_fetch_assoc($result4);
+        $promname = $post4['Name'];
+        $promdesc = $post4['Description'];
+        $promaoff = $post4['AmountOff'];
+        $promtype = $post4['PromoType'];
+        $promcode = $post4['PromoCode'];
+        return new Promotion($promname, $promdesc, $promaoff, $promtype, $promcode);
+    }
+    require ('adEventViewerTable.php');
+
+
+}
+
+?>
---
+++ adEventViewerTable.php
@@ -0,0 +1,111 @@
+<?php
+
+
+
+$code = $_SESSION['event_code'];
+$name = $_SESSION['name'];
+$start =  $_SESSION['start_date'];
+$end = $_SESSION['end_date'];
+$desc =  $_SESSION['description'];
+$promos = $_SESSION['promos'];
+
+
+?>
+
+
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<br>
+<table class="table">
+    <thead>
+    <tr>
+        <th scope="col">Ad Event Code</th>
+        <th scope="col">Ad Event Name</th>
+        <th scope="col">Ad Event Start Date</th>
+        <th scope="col">Ad Event End Date</th>
+        <th scope="col">Description</th>
+
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <th scope="row"><?php echo $code?></th>
+        <td><?php echo $name?></td>
+        <td><?php echo $start?></td>
+        <td><?php echo $end?></td>
+        <td><?php echo $desc?></td>
+    </tbody>
+</table>
+</br>
+<?php
+
+ for ($i = 0; $i < count($promos); $i++){
+      echo '<table class="table">';
+    echo '<thead>';
+    echo '<tr>';
+    echo    '<th scope="col">Promotion Name</th>';
+    echo   '<th scope="col">Description</th>';
+    echo    '<th scope="col">Amount Off</th>';
+    echo    '<th scope="col">Type</th>';
+    echo    '<th scope="col">Promotion Code</th>';
+
+    echo '</tr>';
+    echo '</thead>';
+    echo '<tbody>';
+    echo '<tr>';
+    echo    '<th scope="row">'.getPromotion($promos[$i])->getName().'</th>';
+    echo    '<td>'.getPromotion($promos[$i])->getDescription().'</td>';
+    echo    '<td>'.getPromotion($promos[$i])->getAmountOff().'</td>';
+    echo    '<td>'.getPromotion($promos[$i])->getType().'</td>';
+    echo    '<td>'.getPromotion($promos[$i])->getCode().'</td>';
+    echo '</tbody>';
+    echo '</table>';
+       $promoitem[] = promoitems($promos[$i]['PromoCode']);
+     for ($a = $i; $a < count($promoitem); $a++){
+         echo '<table class="table">';
+         echo '<thead>';
+         echo '<tr>';
+         echo    '<th scope="col">Item Number</th>';
+         echo   '<th scope="col">Item Description</th>';
+         echo    '<th scope="col">Category</th>';
+         echo    '<th scope="col">Department Name</th>';
+         echo    '<th scope="col">Purchase Cost</th>';
+         echo    '<th scope="col">Full Retail Price</th>';
+
+         echo '</tr>';
+         echo '</thead>';
+         echo '<tbody>';
+         echo '<tr>';
+         echo    '<th scope="row">'.getItem($promoitem[$a])->getItemNumber().'</th>';
+         echo    '<td>'.getItem($promoitem[$a])->getDescription().'</td>';
+         echo    '<td>'.getItem($promoitem[$a])->getCategory().'</td>';
+         echo    '<td>'.getItem($promoitem[$a])->getDepartment().'</td>';
+         echo    '<td>'.getItem($promoitem[$a])->getCost().'</td>';
+         echo    '<td>'.getItem($promoitem[$a])->getRetailCost().'</td>';
+         echo '</tbody>';
+         echo '</table>';
+
+
+
+
+
+
+     }
+     echo '</br></br>';
+
+
+}
+
+?>
+</body>
+</html>
